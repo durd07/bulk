256f90eac6db4bc9c1e67d15e6fbd7ace4c62d08
Du Rendong <durd_bitcom@163.com>
2016-01-26 12:31:48 +0800

    add http support

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import os
+import httplib
 
 def update(bin):
     if not os.path.exists(bin):
@@ -7,39 +8,46 @@ def update(bin):
         #command = "scp " + "durd@192.168.1.164:/home/durd/dm8127/svn_jenkins/build_vp_p/" + bin + " ."
         print(command)
         os.system(command)
-    copy2device(ip, bin, "/tmp")
-    run_cmd(ip, "/var/ftp/busybox sh /tmp/" + bin) 
-    run_cmd(ip, "sync") 
+    copy2device(bin, "/tmp")
+    run_cmd("/var/ftp/busybox sh /tmp/" + bin) 
+    run_cmd("sync") 
 
-def run_cmd(ip, cmd):
+def http(cmd):
+    conn = httplib.HTTPConnection(ip, 80)
+    conn.request("GET", "/index.htm")
+    result = conn.getresponse()
+    print(result.status, result.reason)
+
+    data = result.read()
+    print(data)
+    conn.close()
+
+def httpcfg(cmd):
+    conn = httplib.HTTPConnection(ip, 80)
+    conn.request("GET", "/vb.htm?" + cmd)
+    result = conn.getresponse()
+    print(result.status, result.reason)
+
+    data = result.read()
+    print(data)
+    conn.close()
+
+def run_cmd(cmd):
     command = "ssh bit1@" + ip + " \"" + cmd + "\""
     print(command)
     os.system(command)
 
-def copy2device(ip, from_path, to_path):
+def copy2device(from_path, to_path):
     command = "scp " + from_path + " bit1@" + ip + ":" + to_path
     print(command)
     os.system(command)
 
-def copyfromdevice(ip, from_path, to_path):
+def copyfromdevice(from_path, to_path):
     command = "scp " + "bit1@" + ip + ":" + from_path + " " + to_path
     print(command)
     os.system(command)
 
-for item in range(101, 139):
+for item in range(212, 237):
     ip = "192.168.1." + str(item)
-    dev_num = "3702020" + str(item)
-    jiekou_num = "3702020000507" + str(item)
-    park_num = "370202010" + str(item)
-    print("============ " + ip + " = " + dev_num + " = " + jiekou_num + " = " + park_num)
-    run_cmd(ip, "sed -i 's/<device_id>.*<\/device_id>/<device_id>" + dev_num + "<\/device_id>/g' /config/arm_config.xml")
-    run_cmd(ip, "sed -i 's/<deviceID>.*<\/deviceID>/<deviceID>" + dev_num + "<\/deviceID>/g' /config/dsp_config.xml")
-    run_cmd(ip, "sed -i 's/Net_DeviceID=.*/Net_DeviceID=" + dev_num + ";/g' /config/message.txt")
-
-
-    run_cmd(ip, "sed -i 's/<exp_device_id>.*<\/exp_device_id>/<exp_device_id>" + jiekou_num + "<\/exp_device_id>/g' /config/arm_config.xml")
-
-    run_cmd(ip, "sed -i 's/<spot_id>.*<\/spot_id>/<spot_id>" + park_num + "<\/spot_id>/g' /config/arm_config.xml")
-    run_cmd(ip, "sed -i 's/<spotID>.*<\/spotID>/<spotID>" + park_num + "<\/spotID>/g' /config/dsp_config.xml")
-
-    run_cmd(ip, "/sbin/reboot -f &")
+    print("============ " + ip + " ============")
+    httpcfg("timefrequency=-1&datestampenable3=2&sntpip=202.112.7.13&ntpinterval=30")
