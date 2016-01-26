b8f4b49ad27b0af300d5db33ae2f45b77601ab74
Du Rendong <durd_bitcom@163.com>
2016-01-26 10:09:47 +0800

    update

--- test.py
+++ test.py
@@ -26,20 +26,8 @@ def copyfromdevice(ip, from_path, to_path):
     print(command)
     os.system(command)
 
-for item in range(101, 139):
+for item in range(211, 212):
     ip = "192.168.1." + str(item)
-    dev_num = "3702020" + str(item)
-    jiekou_num = "3702020000507" + str(item)
-    park_num = "370202010" + str(item)
-    print("============ " + ip + " = " + dev_num + " = " + jiekou_num + " = " + park_num)
-    run_cmd(ip, "sed -i 's/<device_id>.*<\/device_id>/<device_id>" + dev_num + "<\/device_id>/g' /config/arm_config.xml")
-    run_cmd(ip, "sed -i 's/<deviceID>.*<\/deviceID>/<deviceID>" + dev_num + "<\/deviceID>/g' /config/dsp_config.xml")
-    run_cmd(ip, "sed -i 's/Net_DeviceID=.*/Net_DeviceID=" + dev_num + ";/g' /config/message.txt")
-
-
-    run_cmd(ip, "sed -i 's/<exp_device_id>.*<\/exp_device_id>/<exp_device_id>" + jiekou_num + "<\/exp_device_id>/g' /config/arm_config.xml")
-
-    run_cmd(ip, "sed -i 's/<spot_id>.*<\/spot_id>/<spot_id>" + park_num + "<\/spot_id>/g' /config/arm_config.xml")
-    run_cmd(ip, "sed -i 's/<spotID>.*<\/spotID>/<spotID>" + park_num + "<\/spotID>/g' /config/dsp_config.xml")
-
+    print("============ " + ip + " ============")
+    update("PARK_20160125174403_A1.bin")
     run_cmd(ip, "/sbin/reboot -f &")
