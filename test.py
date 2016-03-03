331923c823ef5edd6d0c7331386c6239e9239fd6
Du Rendong <durd_bitcom@163.com>
2016-03-03 11:41:41 +0800

    add it

--- test.py
+++ test.py
@@ -48,19 +48,19 @@ def copyfromdevice(from_path, to_path):
     print(command)
     os.system(command)
 
-for item in range(211, 212):
+for item in range(101, 139):
 #for item in [112, 114, 116]:
     ip = "192.168.1." + str(item)
     print("\033[95m============ " + ip + " ============\033[0m")
     try:
-        #httpcfg("timefrequency=-1&datestampenable3=2&sntpip=58.58.40.162")
-        #httpcfg("pt_set_save=4,1,0,58.58.40.162,10000,20000,58.58.40.162,18901,3702")
+        httpcfg("timefrequency=-1&datestampenable3=2&sntpip=58.58.40.162")
+        httpcfg("pt_set_save=4,1,0,58.58.40.162,10000,20000,58.58.40.162,18901,3702")
         #httpcfg("network_card_type=4")
         #run_cmd(r"sed -i '1i\ETHTOOL_OPTS=\"speed 10 duplex half autoneg off\"' /mnt/nand/info.cfg")
         #run_cmd("cat /mnt/nand/info.cfg | grep ETH | wc")
         #copy2device("/home/durd/work/ipnc_rdk/target/filesys/opt/ipnc/vd", "/tmp/vd")
-        copy2device("/home/durd/work/ipnc_rdk/ipnc_app/vd/build/vd", "/tmp/vd")
-        run_cmd("mv /tmp/vd /opt/ipnc/")
+        #copy2device("/home/durd/work/ipnc_rdk/ipnc_app/vd/build/vd", "/tmp/vd")
+        #run_cmd("mv /tmp/vd /opt/ipnc/")
         #run_cmd("/opt/ipnc/pid_status  -q|awk \'NR>4&&(\$6>120||\$8>100) {print  \$4 \\\"\\t\\\" \$6 \\\"\\t\\\" \$8 \\\"\\t\\\" \$9 }\' ")
         #run_cmd("/opt/ipnc/pid_status  -q|awk \'{print  \$4 \\\"\\t\\\" \$6 \\\"\\t\\\" \$8 \\\"\\t\\\" \$9 }\' ")
         #run_cmd("uptime")
@@ -78,4 +78,6 @@ for item in range(211, 212):
 Pre defined actions
 1.  set ntp server 
     httpcfg("timefrequency=-1&datestampenable3=2&sntpip=58.58.40.162")
+2.  set platform for zehin
+    httpcfg("pt_set_save=4,1,0,58.58.40.162,10000,20000,58.58.40.162,18901,3702")
 """
